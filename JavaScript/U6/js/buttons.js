d25920fe12dbe39c7a84b21f311317bd8d530e50
lucastrike <lucastrike1@gmail.com>
2016-11-11 12:30:15 +0100

    Added animation slides and validation plugin

---
+++ JavaScript/U6/js/buttons.js
@@ -0,0 +1,21 @@
+
+$("document").ready(function() {
+  alert("Web page is loaded!!!");
+
+  /*$("#btnshow").on('click', function(){
+    $("#image").show(1000);
+  });
+
+  $("#btnhide").on('click', function(){
+    $("#image").hide(1000);
+  });
+
+  $("#btntoggle").on('click', function(){
+    $("#image").toggle(1000, function(){
+      alert("Callback");
+    });
+  });*/
+
+  
+
+});
