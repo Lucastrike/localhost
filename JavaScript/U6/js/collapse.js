561e2e8ad8d22ed40c8ebd351758ca85d89b2a6e
lucastrike <lucastrike1@gmail.com>
2016-11-16 12:56:43 +0100

    Updated animation exercise

--- JavaScript/U6/js/collapse.js
+++ JavaScript/U6/js/collapse.js
@@ -8,12 +8,18 @@ $(document).ready(function() {
     });
   });
 
-  $(".box1").on('click', function(){
-    $(this).animate({'margin-left':'0px', 'margin-top':'0px'},2000);
-  });
-  $(".box2").on('click', function(){
-    $(this).animate({'margin-left':'0px', 'margin-bottom':'0px'},2000);
-  });
+
+
+    $(".box2").draggable();
+    $(".box1").droppable({
+      drop: function(event, ui) {
+        $(this).css("background-color","orange");
+      },
+      out: function(event, ui) {
+        $(this).css("background-color","green");
+      }
+    });
+
   $(".box3").on('click', function(){
     $(this).animate({'margin-left':'0px', 'margin-bottom':'0px'},2000);
   });
