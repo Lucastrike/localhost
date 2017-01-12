29f7d3c354c63cf6f7b3f4c63935718eafb55ae7
lucastrike <lucastrike1@gmail.com>
2017-01-12 13:53:38 +0100

    updated php porject

--- PHP/U4/shopper/insertar.php
+++ PHP/U4/shopper/insertar.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", "1");
 
 include ("connection.php");
 
@@ -10,9 +12,10 @@ $pass = md5($password);
 $query = mysqli_query($connection,"SELECT email FROM usuario_tienda WHERE email='$email';");
 
 if (mysqli_num_rows($query) == 0) {
-  mysqli_query($connection, "INSERT INTO `usuario_tienda`(`nombre`, `email`, `password`) VALUES ('$username','$email','$pass');")
+  mysqli_query($connection, "INSERT INTO usuario_tienda (`nombre`, `email`, `password`) VALUES ('$username','$email','$pass');");
+  echo " Bienvenido"." ".$username;
+} else {
+  echo " Usuario ya existente";
 }
-
-echo "Bienvenido";
 
  ?>
